401461fcbbf99eb6de85f7c403570dff66ad9041
biraj buddhacharya <birajbuddhacharya@gmail.com>
2025-05-24 14:54:25 +0545

    Optimization: Fixed inefficient getAllUser endpoing

--- backend/api/controllers/registration/getAllRegistrationCount.py
+++ backend/api/controllers/registration/getAllRegistrationCount.py
@@ -1,13 +1,8 @@
 from api.models.registration import Registration
 from sqlalchemy import func
-import time
-from api.utils.logger import Logger
 
 def GetAllRegistrationCount():
-    start = time.time()
     counts = Registration.query.group_by(Registration.event_id)\
         .with_entities(Registration.event_id, func.count())\
         .all()
-    end = time.time()
-    Logger.info(f"response time: {end - start:.3f}s")
     return {"counts": dict(counts)}
--- backend/api/controllers/user/get_all_user.py
+++ backend/api/controllers/user/get_all_user.py
@@ -5,25 +5,32 @@ from firebase_admin import auth
 
 def get_all_user_controller():
     try:
-        users = User.query.order_by(User.role.desc()).all()
+        users = User.query.all()
+        users_lookup = {user.fid: user for user in users}
         
         # Format user data
         user_list = []
-        for user in users:
-            # Get Firebase user info
-            try:
-                firebase_user = auth.get_user(user.fid)
-                user_data = {
-                    "fid": user.fid, 
-                    "email": firebase_user.email,
-                    "imageUrl": firebase_user.photo_url,
-                    "createdAt": firebase_user.user_metadata.creation_timestamp,
-                    "role": user.role
-                }
-                user_list.append(user_data)
-            except Exception as firebase_error:
-                Logger.error(f"Error getting Firebase user info: {str(firebase_error)}")
-                continue
+        page = auth.list_users()
+        while page: 
+            for firebase_user in page.users: 
+                try:
+                    # retriving index of user in users list
+                    user = users_lookup.get(firebase_user.uid, None)
+                    if user is None: 
+                         continue
+                    
+                    user_data = {
+                        "fid": user.fid, 
+                        "email": firebase_user.email,
+                        "imageUrl": firebase_user.photo_url,
+                        "createdAt": firebase_user.user_metadata.creation_timestamp,
+                        "role": user.role
+                    }
+                    user_list.append(user_data)
+                except Exception as firebase_error:
+                    Logger.error(f"Error getting Firebase user info: {str(firebase_error)}")
+                    continue
+            page = page.get_next_page()
             
         return jsonify(user_list)
 
