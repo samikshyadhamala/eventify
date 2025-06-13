40662828827aa99654a783b1cfbf70f0cac6a86e
Biraj Buddhacharya <130218032+BirajBuddhacharya@users.noreply.github.com>
2025-06-13 13:39:56 +0545

    logout bug fix 2

--- backend/api/controllers/auth/logout.py
+++ backend/api/controllers/auth/logout.py
@@ -1,10 +1,25 @@
 from flask import request, jsonify, make_response
-import os 
-import dotenv; dotenv.load_dotenv()
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
 
 def logout():
-    is_production = os.getenv('FLASK_ENV') == 'production'
+    is_production = os.getenv("FLASK_ENV") == "production"
     response = make_response("Cookie cleared!")
-    response.delete_cookie('loginToken', secure=is_production)
-    response.delete_cookie('refreshToken', secure=is_production)
+    response.delete_cookie(
+        "loginToken",
+        path="/",
+        secure=is_production,
+        samesite="None" if is_production else "Lax",
+        httponly=True,
+    )
+    response.delete_cookie(
+        "refreshToken",
+        path="/",
+        secure=is_production,
+        samesite="None" if is_production else "Lax",
+        httponly=True,
+    )
     return response
