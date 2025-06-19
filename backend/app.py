3f87e69cdaefb55aa488766104cb2dd039db8932
Biraj Buddhacharya <130218032+BirajBuddhacharya@users.noreply.github.com>
2025-06-19 23:43:33 +0545

    port change

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from api import create_app
 from api.utils.logger import Logger
+import os 
+import dotenv 
+dotenv.load_dotenv()
+
 if __name__ == "__main__":
     app = create_app()
+    port = int(os.getenv("PORT", "4000"))
     try: 
-        app.run(debug=True, host="0.0.0.0", port=5000)
+        app.run(debug=True, host="0.0.0.0", port=port)
     except Exception as e: 
         Logger.error(e)
