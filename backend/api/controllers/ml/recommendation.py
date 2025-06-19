e01e5acd191a2507a0bbeb9dbea7beb910777d4c
Biraj Buddhacharya <130218032+BirajBuddhacharya@users.noreply.github.com>
2025-06-19 23:17:00 +0545

    render deployment out of memory fix

--- backend/api/controllers/ml/recommendation.py
+++ backend/api/controllers/ml/recommendation.py
@@ -1,9 +1,11 @@
 from models.recommendation import EventRecommendationModel
 from api import db
+from api.models.event import Event
 
 # preloadding model 
-model = EventRecommendationModel(session=db.session)
+# model = EventRecommendationModel(session=db.session)
 
 def Recommendation(): 
-    recommendations = model.recommend_events(pastEvents=[], k=10)
-    return recommendations, 200
+    # recommendations = model.recommend_events(pastEvents=[], k=10)
+    recommendations = Event.query.order_by(Event.created_at.desc()).limit(10).all()
+    return [event.event_id for event in recommendations], 200
